35e8cc5c93dd4bd69e9b15f181dbe084071d9134
moisterchief <mv119@students.waikato.ac.nz>
2025-07-31 12:10:46 +1200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// server.js
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
