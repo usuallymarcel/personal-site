9ae64928f4e27ed9100208938feece1eed30f73f
Marcel Van Eyk <marcelve2112@gmail.com>
2025-08-01 20:58:51 +1200

    serve static html

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/Hello', (req, res) => {
   res.send('Hello');
 });
 
